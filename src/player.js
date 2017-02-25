c456300bed8a86846b96a3f590bc96d51c800e5a
Vladimir Volchkov <admin@howtoclient.com>
2017-02-25 17:46:11 +0200

    added webpack build/watch configurations

--- src/player.js
+++ src/player.js
@@ -1,3 +1,15 @@
 /**
  * Created by vladi on 21-Feb-17.
  */
+export default class VidoolooPlayer {
+    constructor() {
+        console.log("a test is happening");
+        throw new Error(); // check source maps
+        console.log("a test is happening");
+        console.log("a test is happening");
+        console.log("a test is happening");
+        console.log("a test is happening");
+    }
+}
+
+//need to take over <script> positioning
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,50 @@ const path = require('path');
 const webpack = require('webpack');
 module.exports = {
     context: path.resolve(__dirname, './src'),
+    devServer: {
+        contentBase: __dirname + '/test-suite', // `__dirname` is root of the project
+        compress: true,
+    },
     entry: {
+        //home: './home.js',
+        //events: './events.js',
+        //contact: './contact.js',
         app: './player.js'
     },
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+            }
+        ]
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            debug: true
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('development')
+            }
+        })
+    ]
+
+    /*plugins: [
+     new webpack.optimize.CommonsChunkPlugin({
+     name: 'commons',
+     filename: 'commons.js',
+     minChunks: 2,
+     }),
+     ]*/
+    /*plugins: [ //extract-text-webpack-plugin
+     new ExtractTextPlugin({
+     filename: '[name].bundle.css',
+     allChunks: true,
+     }),
+     ],*/
 };
