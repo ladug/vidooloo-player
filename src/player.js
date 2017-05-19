4abf67039e4727c069bd9737b0c27d27df2fccee
Vladimir Volchkov <admin@howtoclient.com>
2017-05-19 20:27:24 +0200

    updated basic dev config

--- src/player.js
+++ src/player.js
@@ -4,12 +4,17 @@
 export default class VidoolooPlayer {
     constructor() {
         console.log("a test is happening");
+        console.log("a test is happening");
         throw new Error(); // check source maps
         console.log("a test is happening");
         console.log("a test is happening");
         console.log("a test is happening");
         console.log("a test is happening");
+
     }
 }
-
+/*place the player in the position of the script tag*/
+const currentScript = document.currentScript;
+new VidoolooPlayer(currentScript); //pass script tag to the player to use for configurations
+currentScript.parentNode.removeChild(currentScript);
 //need to take over <script> positioning
