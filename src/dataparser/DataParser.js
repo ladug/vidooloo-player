0a30ab5771549c4c606aada3f662df1f46145ae6
Vladimir Volchkov <admin@howtoclient.com>
2017-05-27 19:29:06 +0200

    minor tweaks

--- src/dataparser/DataParser.js
+++ src/dataparser/DataParser.js
@@ -2,14 +2,17 @@
  * Created by vladi on 27-May-17.
  */
 import EventEmitter from "../events/EventEmitter";
+import PvfReader from "../readers/PvfReader";
+import SvfReader from "../readers/SvfReader";
+export default class DataParser extends EventEmitter {
+    svfBytes = null;
+    pvfBytes = null;
 
-export default class DataParser extends EventEmitter{
-    svfBytes=null;
-    pvfBytes=null;
-    addSvfChunk(chunk){
+    addSvfChunk(chunk) {
 
     }
-    addPvfChunk(chunk){
+
+    addPvfChunk(chunk) {
 
     }
 }
