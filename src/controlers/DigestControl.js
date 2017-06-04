6e73f5715d1acdb76589bb4d81d904669fe103ad
Vladimir Volchkov <admin@howtoclient.com>
2017-06-05 00:42:43 +0200

    updated DigestControll

--- src/controlers/DigestControl.js
+++ src/controlers/DigestControl.js
@@ -4,7 +4,7 @@
 import EventEmitter from "../events/EventEmitter";
 import DataParser from "../dataparser/DataParser";
 import {ChunkDownloadedEvent} from "../downloadmanager/DownloadManagerEvents";
-import {MissingHeaderEvent} from "../dataparser/DataParserEvents";
+import {} from "../dataparser/DataParserEvents";
 import {HeadersEvent} from "./DigestControlEvents";
 import {assert} from "../common";
 
@@ -21,25 +21,20 @@ export default class DigestControl extends EventEmitter {
             ...this.configurations,
             ...configurations
         };
-        this.dataParser.addEventListener(MissingHeaderEvent, this._onSvfMissingData)
         this.pvfDownloadManager = pvfDownloadManager;
         this.pvfDownloadManager.addEventListener(ChunkDownloadedEvent, this._onPvfChunk);
         this.svfDownloadManager = svfDownloadManager;
         this.svfDownloadManager.addEventListener(ChunkDownloadedEvent, this._onSvfChunk);
     }
 
-    _onSvfMissingData = () => {
-        this.svfDownloadManager.readChunk(); //event can only be fired at the end, i think
-    };
-
     _onPvfChunk = (event) => {
         console.log("_onPvfChunk", event);
-        this.dataParser.addPvfChunk(new Uint8Array(event.chunk), event.chunkData);
+        this.dataParser.addPvfChunk(new Uint8Array(event.chunk));
     };
 
     _onSvfChunk = (event) => {
         console.log("_onSvfChunk", event);
-        this.dataParser.addSvfChunk(new Uint8Array(event.chunk), event.chunkData);
+        this.dataParser.addSvfChunk(new Uint8Array(event.chunk));
     };
 
     start() {
